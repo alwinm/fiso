b85502bfef1651efbe1e71612d78ce5d4018c1fc
alwinm <alwin.mao@pitt.edu>
2022-10-18 18:22:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 setup(name='fiso',
       version='0.0',
-      packages=[''],
+      packages=['boundary','tools','walker','ext','setups','fiso','fiso_beta','test_fiso'],
       )
