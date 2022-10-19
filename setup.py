1839452328460e598ac7dd71d640c1c132797c5d
alwinm <alwin.mao@pitt.edu>
2022-10-18 18:17:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from distutils.core import setup
 setup(name='fiso',
       version='0.0',
-      py_modules=['fiso'],
-      packages=['fiso'],
-      package_dir={'fiso': '.'},
+      packages=[''],
       )
