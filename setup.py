91d34d4ff242cc8cb1caf8245bb1e639760c757b
alwinm <alwin.mao@pitt.edu>
2022-10-18 19:56:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='fiso',
       version='0.0',
-      packages=find_packages(where='..'),
+      packages=find_packages(where='.'),
       package_dir={"fiso":"./"},
       )
